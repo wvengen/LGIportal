e73945dfa385f0ae7f1e11a653775f06f5caf6c2
wvengen <wvengen@nikhef.nl>
2011-08-11 12:14:11 +0200

    also update index.php for Dwoo change

--- lgi/index.php
+++ lgi/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Include the main class, the rest will be automatically loaded
-require_once 'Dwoo/dwooAutoload.php';
+require_once 'php/utilities/dwoo.php';
 require_once 'php/utilities/sessions.php';
 require_once 'php/utilities/data.php';
 require_once 'lgi.config.php';
@@ -17,7 +17,7 @@ else
          //check which authentication mechanism
           if(strcmp(_AUTH_MECHANISM_,"DATABASE")==0)
           {
-               $dwoo =new Dwoo();
+               $dwoo =new LGIDwoo();
                $dwoo->output('dwoo/login.tpl',createDwooData());
           }
           else if(strcmp(_AUTH_MECHANISM_,"DIGEST")==0)
