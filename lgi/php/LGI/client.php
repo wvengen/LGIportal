bb89d22214f77e5c809e34e0d638e31a69d23373
wvengen <wvengen@nikhef.nl>
2011-08-23 17:52:19 +0200

    rewrite lgijob classes and rename to LGI

---
+++ lgi/php/LGI/client.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * LGI client
+ *
+ * @author wvengen
+ * @package lgijob
+ */
+require_once(dirname(__FILE__).'/connection.php');
+
+class LGIClientException extends LGIException { }
+
+/** LGI client
+ */
+class LGIClient extends LGIConnection
+{
+	function __construct($url=null, $project=null, $user=null, $groups=null, $certificate=null, $privatekey=null, $ca_chain=null)
+	{
+		// defaults from configuration in ~/.LGI
+		$cfgdir = ((strtolower(substr(PHP_OS,0,3))=='win') ? getenv('USERPROFILE'): getenv('HOME')) .
+			DIRECTORY_SEPARATOR . '.LGI' . DIRECTORY_SEPARATOR;
+		if ($user===null)
+			$user = trim(@file_get_contents($cfgdir.'user'));
+		if ($groups===null)
+			$groups = trim(@file_get_contents($cfgdir.'groups'));
+		if ($url===null)
+			$url = trim(@file_get_contents($cfgdir.'defaultserver'));
+		if ($project===null)
+			$project = trim(@file_get_contents($cfgdir.'defaultproject'));
+		if ($certificate===null)
+			$certificate = $cfgdir.'certificate';
+		if ($privatekey===null)
+			$privatekey = $cfgdir.'privatekey';
+		if ($ca_chain===null)
+			$ca_chain = $cfgdir.'ca_chain';
+
+		if (strlen($user)==0)
+			throw new LGIException("No LGI user specified");
+		if (strlen($groups)==0)
+			throw new LGIException("No LGI groups specified");
+		if (strlen($project)==0)
+			throw new LGIException("No LGI project specified");
+
+		$this->project = $project;
+		$this->user = $user;
+		$this->groups = $groups;
+
+		parent::__construct($url, $certificate, $privatekey, $ca_chain);
+	}
+
+	function jobList($application=null, $state=null, $start=null, $limit=null)
+	{
+		$args = array(
+			'project' => $this->project,
+			'user' => $this->user,
+			'groups' => $this->groups,
+		);
+		if ($application!==null) $args['application'] = $application;
+		if ($state!==null) $args['state'] = $state;
+		if ($start!==null) $args['start'] = $start;
+		if ($limit!==null) $args['limit'] = $limit;
+		$ret = $this->postToServer('/interfaces/interface_job_state.php', $args);
+		$ret = $ret['response'];
+		if (!array_key_exists('job', $ret)) $ret['job'] = array();
+		elseif (!is_array($ret['job'])) $ret['job'] = array($ret['job']);
+		return $ret;
+	}
+
+	function jobState($job_id)
+	{
+		$args = array(
+			'project' => $this->project,
+			'user' => $this->user,
+			'groups' => $this->groups,
+		);
+		if ($job_id!==null) $args['job_id'] = $job_id;
+		$ret = $this->postToServer('/interfaces/interface_job_state.php', $args);
+		$ret = $ret['response'];
+		if (!array_key_exists('job', $ret)) $ret['job'] = array();
+		return $ret;
+	}
+
+	function jobDelete($job_id)
+	{
+		$args = array(
+			'project' => $this->project,
+			'user' => $this->user,
+			'groups' => $this->groups,
+		);
+		if ($job_id!==null) $args['job_id'] = $job_id;
+		$ret = $this->postToServer('/interfaces/interface_delete_job.php', $args);
+		return $ret['response'];
+	}
+
+	function jobSubmit($application, $input=null, $target_resources='any', $write_access=null, $read_access=null, $files=array())
+	{
+		$args = array(
+			'project' => $this->project,
+			'user' => $this->user,
+			'groups' => $this->groups,
+			'number_of_uploaded_files' => count($files),
+		);
+		if ($application!==null) $args['application'] = $application;
+		if ($target_resources!==null) $args['target_resources'] = $target_resources;
+		if ($write_access!==null) $args['write_access'] = $write_access;
+		if ($read_access!==null) $args['read_access'] = $read_access;
+		if ($input!==null) $args['input'] = bin2hex($input);
+		$fileargs = array();
+		foreach ($files as $f) $fileargs['uploaded_file_'.(count($fileargs)+1)] = $f;
+		$ret = $this->postToServer('/interfaces/interface_submit_job.php', $args, $fileargs);
+		return $ret['response'];
+	}
+
+	function resourceList()
+	{
+		$args = array(
+			'project' => $this->project,
+			'user' => $this->user,
+			'groups' => $this->groups,
+		);
+		$ret = $this->postToServer('/interfaces/interface_project_resource_list.php', $args);
+		$ret = $ret['response'];
+		if (!array_key_exists('resource', $ret)) $ret['resource'] = array();
+		elseif (!is_array($ret['resource'])) $ret['resource'] = array($ret['resource']);
+		return $ret;
+	}
+
+	function serverList()
+	{
+		$args = array(
+			'project' => $this->project,
+			'user' => $this->user,
+			'groups' => $this->groups,
+		);
+		$ret = $this->postToServer('/interfaces/interface_project_server_list.php', $args);
+		$ret = $ret['response'];
+		if (!array_key_exists('project_server', $ret)) $ret['project_server'] = array();
+		elseif (!is_array($ret['project_server'])) $ret['project_server'] = array($ret['project_server']);
+		return $ret;
+	}
+}
+
+?>
---
+++ lgi/php/LGI/connection.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Base LGI connection
+ *
+ * @author wvengen
+ * @package lgijob
+ */
+
+/** Base LGI exception */
+class LGIException extends Exception { }
+/** Exception in LGI connection */
+class LGIConnectionException extends LGIException { }
+/** Exception for LGI server error */
+class LGIServerException extends LGIConnectionException { }
+
+/** Base class for communicating with an LGI server.
+ *
+ * This takes care of authentication, posting, xml parsing and basic error handling.
+ */
+class LGIConnection
+{
+	private $curlh;
+
+	protected $url;
+
+	function __construct($url, $certificate, $privatekey, $ca_chain)
+	{
+		$this->curlh = null;
+		$this->url = $url;
+		$this->certificate = $certificate;
+		$this->privatekey = $privatekey;
+		$this->ca_chain = $ca_chain;
+	}
+
+	function connect()
+	{
+		// some checks first
+		$this->check_file($this->certificate, 'LGI certificate');
+		$this->check_file($this->privatekey, 'LGI private key');
+		$this->check_file($this->ca_chain, 'LGI CA chain');
+		if (!$this->url)
+			throw new LGIException('No server url specified');
+		if (strtolower(substr($this->url,0,6))!='https:')
+			throw new LGIException('Server must be an https url');
+		// then initialise
+		$this->curlh = curl_init();
+		curl_setopt($this->curlh, CURLOPT_SSLCERT, $this->certificate);
+		curl_setopt($this->curlh, CURLOPT_SSLKEY, $this->privatekey);
+		curl_setopt($this->curlh, CURLOPT_CAINFO, $this->ca_chain);
+		curl_setopt($this->curlh, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($this->curlh, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($this->curlh, CURLOPT_NOSIGNAL, true);
+		curl_setopt($this->curlh, CURLOPT_VERBOSE, true); // by default silent
+		curl_setopt($this->curlh, CURLOPT_NOPROGRESS, true);
+		curl_setopt($this->curlh, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($this->curlh, CURLOPT_RETURNTRANSFER, true);
+		// we handle errors ourselves because curl doesn't show the result on error
+		curl_setopt($this->curlh, CURLOPT_FAILONERROR, false);
+	}
+
+	function close()
+	{
+		curl_close($this->curlh);
+		$this->curlh = null;
+	}
+
+	protected function postToServer($apipath, $variables=array(), $files=array(), $path=null)
+	{
+		if (!$this->curlh) $this->connect();
+		// be safe when settings variables
+		function escape_at($s) { return ($s[0]=='@') ? '&#64;'.substr($s,1) : $s; }
+		$variables = array_map('escape_at', $variables);
+		// file uploads as special postfields
+		function file_argify($s) { return '@'.$s; }
+		$files = array_map('file_argify', $files);
+		// perform request
+		curl_setopt($this->curlh, CURLOPT_URL, $this->url . $apipath);
+		curl_setopt($this->curlh, CURLOPT_POST, true);
+		curl_setopt($this->curlh, CURLOPT_POSTFIELDS, array_merge($variables, $files));
+		$result = curl_exec($this->curlh);
+		if (curl_errno($this->curlh) > 0) {
+			throw new LGIConnectionException(sprintf('cURL error %d: %s',
+				curl_errno($this->curlh), curl_error($this->curlh)), curl_errno($this->curlh));
+		}
+		if (($httpcode=curl_getinfo($this->curlh, CURLINFO_HTTP_CODE)) >= 400) {
+			throw new LGIConnectionException(sprintf('Server error %d', $httpcode), $httpcode);
+		}
+		$resp = json_decode(json_encode(simplexml_load_string($result)), TRUE);
+		if (in_array('error', $resp['response'])) {
+			$err = $resp['response']['error'];
+			throw new LGIServerException(sprintf('LGI error %d: %s',
+				$err['number'], $err['message']), $err['number']);
+		}
+		return $resp;
+	}
+
+	protected function check_file($file, $desc)
+	{
+		if (!file_exists($file)) {
+			user_error($desc.' not found: '.$file);
+			throw new LGIException($desc.' not found');
+		}
+		if (!is_readable($file)) {
+			user_error($desc.' found but not readable: '.$file);
+			throw new LGIException($desc.' not readable');
+		}
+	}
+}
+
+?>
