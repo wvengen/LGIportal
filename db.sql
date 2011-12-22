6e8c621d45964dfff85c622e07748ca2dc394353
wvengen <wvengen@nikhef.nl>
2011-12-22 14:10:04 +0100

    new sql structure

--- db.sql
+++ db.sql
@@ -1,19 +1,60 @@
-/*
- * Initial database for LGIportal
- */
+-------
+-----
+----- LGIportal database structure
+-----
+-------
 
+
+---
+--- users and their properties
+---
 CREATE TABLE users (
-	userId        VARCHAR(20) PRIMARY KEY,
-	passwordHash  VARCHAR(150),
-	salt          VARCHAR(50)
+	id            INTEGER PRIMARY KEY AUTO_INCREMENT,
+	--- username
+	name          VARCHAR(20) PRIMARY KEY,
+	--- local password
+	passwd_hash   VARCHAR(150),
+	--- any other authentication mechanisms can add fields here
 );
 
-/* user 'demo' with password 'demo' */
-INSERT INTO users SET userId='demo', passwordHash='e77b0f5cad5cf53cece557d8111c72393844b3a960605af028efebe7f9ebb61f3b1ffae674a7cf959a9bf09e8f439adf0682cdf1cad44e1bdc552be1eee394a7', salt='abc';
 
-CREATE TABLE usercertificates (
-	userId        VARCHAR(20),
-	certificate   TEXT,
-	userkey       TEXT
+---
+--- users' LGI credentials
+---   also contains data about the certificate because PHP has no
+---   built-in certificate parsing functions; and this allows us
+---   to make more sophisticated queries :)
+---
+CREATE TABLE usercerts (
+	id            INTEGER PRIMARY KEY AUTO_INCREMENT,
+	--- username
+	userid        INTEGER REFERENCES users(id),
+	--- user's certificate and key (paths to files for now)
+	cert          TEXT,
+	key           TEXT,
+	--- username as present in the certificate
+	username      VARCHAR(20),
+	--- whether the user has a number of fixed groups, or can choose any
+	---   when the certificate has defined groups, this should be TRUE
+	fixedgroups   BOOLEAN
+);
+
+---
+--- groups a user can choose
+---   When a certificate has fixedgroups TRUE, these are the groups that
+---   are present in the certificate. Else these are groups the user
+---   may choose from the user-interface.
+---
+CREATE TABLE usergroups (
+	usercertid    INTEGER REFERENCES usercerts(id),
+	name          VARCHAR(20)
+);
+
+---
+--- projects a user can choose
+---   Should be populated from a users' certificate
+---
+CREATE TABLE userprojects (
+	usercertid    INTEGER REFERENCES usercerts(id),
+	name          VARCHAR(20)
 );
 
