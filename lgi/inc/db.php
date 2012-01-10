bb86e3a809ac98f35f7908f5767832e03fb98e47
wvengen <wvengen@nikhef.nl>
2012-01-10 15:37:50 +0100

    rename mysql error function

--- lgi/inc/db.php
+++ lgi/inc/db.php
@@ -17,8 +17,8 @@ function lgi_mysql_connect() {
 	global $lgi_mysql_connection;
 	// setup
 	if ($lgi_mysql_connection==NULL)
-		$lgi_mysql_connection = mysql_connect(config('MYSQL_SERVER'), config('MYSQL_USER'), config('MYSQL_PASSWORD')) or showDBError();
-	mysql_select_db(config('MYSQL_DBNAME'), $lgi_mysql_connection) or showDBError();
+		$lgi_mysql_connection = mysql_connect(config('MYSQL_SERVER'), config('MYSQL_USER'), config('MYSQL_PASSWORD')) or lgi_mysql_throw();
+	mysql_select_db(config('MYSQL_DBNAME'), $lgi_mysql_connection) or lgi_mysql_throw();
 }
 
 /** Perform a query on the LGI database with proper escaping; sprintf-style function.
@@ -38,14 +38,14 @@ function lgi_mysql_query() {
 	$fmt = preg_replace('/%%/', '%s', $fmt);
 	array_walk($args, create_function('&$v', '$v=mysql_real_escape_string($v);'));
 	$result = mysql_query(vsprintf($fmt, $args));
-	if ($result === false) showDBError();
+	if ($result === false) lgi_mysql_throw();
 	return $result;
 }
 
 /**
  * Throw and log MySQL database error.
  */
-function showDBError() {
+function lgi_mysql_throw() {
   error_log("MySQL error: ".mysql_error());
   throw new LGIPortalException("Server Error. Please contact web administrator.");
 }
